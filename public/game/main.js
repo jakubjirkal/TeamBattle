e21dbae4859a4bd7939706653293893115856417
Jakub Jirkal <jirkal.jakub@gmail.com>
2018-12-04 20:49:36 +0100

    Move added

--- public/game/main.js
+++ public/game/main.js
@@ -1,9 +1,16 @@
 let battleField = undefined;
+let me = undefined;
 
 const socket = io();
 
 const dimension = 800;
 const diff = dimension/100;
+let moves = {
+  up: false,
+  left: false,
+  down: false,
+  right: false
+};
 
 function setup() {
     createCanvas(dimension+200, dimension);
@@ -16,6 +23,40 @@ function draw() {
     drawEdges();
     drawTitle();
     if(battleField !== undefined) drawBattleField();
+    if(me !== undefined) drawPlayer(me);
+
+    checkKeys();
+}
+
+function keyPressed(){
+    setMove(keyCode, true);
+}
+
+function keyReleased(){
+    setMove(keyCode, false);
+}
+
+function setMove(keyCode, move){
+    if(keyCode === 87){ //UP
+        moves.up = move;
+    }else if(keyCode === 65){ //LEFT
+        moves.left = move;
+    }else if(keyCode === 83){ //DOWN
+        moves.down = move;
+    }else if(keyCode === 68){ //RIGHT
+        moves.right = move;
+    }
+}
+
+function checkKeys(){
+    if(moves.up && moves.right) processKeyPress({x: 1, y: -1});
+    else if(moves.up && moves.left) processKeyPress({x: -1, y:-1});
+    else if(moves.down && moves.right) processKeyPress({x: 1, y:1});
+    else if(moves.down && moves.left) processKeyPress({x: -1, y: 1});
+    else if(moves.up) processKeyPress({x: 0, y: -1});
+    else if(moves.left) processKeyPress({x: -1, y: 0});
+    else if(moves.down) processKeyPress({x: 0, y: 1});
+    else if(moves.right) processKeyPress({x: 1, y: 0});
 }
 
 function drawEdges(){
@@ -63,8 +104,22 @@ function drawBattleField(){
     }
 }
 
+function drawPlayer(player){
+    if(player.team === 1) fill(color(255,0,0));
+    else fill(color(0,0,128));
+
+    ellipse(player.x, player.y, 20);
+}
 
 //SOCKET STUFF
+function processKeyPress(dirs){
+    socket.emit('mouseProcess', dirs);
+}
+
 socket.on('battleField', (data) => {
     battleField = data;
+});
+
+socket.on('me', (data) => {
+   me = data;
 });
--- server/Game.js
+++ server/Game.js
@@ -1,3 +1,5 @@
+const getDistance = require('./commonOperations').getDistance;
+
 class Game{
     constructor(app){
         this.server = require('http').createServer(app);
@@ -7,6 +9,7 @@ class Game{
         this.battleField = require('./data/battlefield');
 
         this.SOCKET_LIST = {};
+        this.PLAYER_LIST = {};
         this.id = 1;
     }
 
@@ -23,13 +26,61 @@ class Game{
             this.id++;
             this.SOCKET_LIST[socket.id] = socket;
             console.log('Socket connection with id: ' + socket.id);
-
+            this.setNewPlayer(1, socket.id);
             socket.emit('battleField', this.battleField);
+
+            socket.on('mouseProcess', (data) => {
+                this.movePlayer(socket.id, data);
+            });
 
             socket.on('disconnect', () => {
                 console.log('Socket dissconnected with id: ' + socket.id);
             });
         });
+
+            //60FPS
+        setInterval(() => {
+            for(let pi in this.PLAYER_LIST){
+                const player = this.PLAYER_LIST[pi];
+                const socket = this.SOCKET_LIST[player.id];
+
+                socket.emit('me', player);
+            }
+        }, 1000/60);
+    }
+
+    setNewPlayer(team, id){
+        const player = {
+            team,
+            id,
+            hp: 100,
+            ammo: 10,
+            x: 400,
+            y: team === 1 ? 100 : 700
+        };
+
+        this.PLAYER_LIST[id] = player;
+    }
+
+    movePlayer(id, dirs){
+        const newX = this.PLAYER_LIST[id].x + dirs.x;
+        const newY = this.PLAYER_LIST[id].y + dirs.y;
+
+        let possible = true;
+
+        //check edges
+        if(newX <= 19 || newX >= 781 || newY <= 19 || newY >= 781) possible = false;
+
+        //check battleField
+        for(let i = 0; i < this.battleField.length; i++){
+            const dist = getDistance(newX, newY, this.battleField[i].x, this.battleField[i].y);
+            if(dist <= 16) possible = false;
+        }
+
+        if(possible){
+            this.PLAYER_LIST[id].x = newX;
+            this.PLAYER_LIST[id].y = newY;
+        }
     }
 }
 
