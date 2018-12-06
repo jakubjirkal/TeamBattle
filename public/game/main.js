81fd97319d8f269da6c744f15cb71ddb45bae732
Jakub Jirkal <jirkal.jakub@gmail.com>
2018-12-06 21:27:29 +0100

    Shooting working

--- public/game/main.js
+++ public/game/main.js
@@ -1,11 +1,12 @@
 let battleField = undefined;
 let me = undefined;
 let otherPlayers = undefined;
+let bullets = undefined;
 
 const socket = io();
 
 const dimension = 800;
-const diff = dimension/100;
+const diff = dimension / 100;
 let moves = {
   up: false,
   left: false,
@@ -14,130 +15,165 @@ let moves = {
 };
 
 function setup() {
-    createCanvas(dimension+200, dimension);
+  createCanvas(dimension + 200, dimension);
 
-    const name = prompt('Select your name');
-    const team = Number(prompt('Select your team (1-2)'));
+  const name = prompt("Select your name");
+  const team = Number(prompt("Select your team (1-2)"));
 
-    processNameAndTeam(name, team);
+  processNameAndTeam(name, team);
 }
 
 function draw() {
-    background(0);
+  background(0);
 
-    drawEdges();
-    drawTitle();
-    if(battleField !== undefined) drawBattleField();
-    if(me !== undefined) drawPlayer(me);
-    if(otherPlayers !== undefined){
-        for(let i = 0; i < otherPlayers.length; i++) drawPlayer(otherPlayers[i]);
-    }
+  drawEdges();
+  drawTitle();
+  if (battleField !== undefined) drawBattleField();
+  if (me !== undefined) drawPlayer(me);
+  if (otherPlayers !== undefined) {
+    for (let i = 0; i < otherPlayers.length; i++) drawPlayer(otherPlayers[i]);
+  }
+  if (bullets !== undefined) drawBullets();
 
-    checkKeys();
+  checkKeys();
 }
 
-function keyPressed(){
-    setMove(keyCode, true);
+function keyPressed() {
+  setMove(keyCode, true);
 }
 
-function keyReleased(){
-    setMove(keyCode, false);
+function keyReleased() {
+  setMove(keyCode, false);
 }
 
-function setMove(keyCode, move){
-    if(keyCode === 87){ //UP
-        moves.up = move;
-    }else if(keyCode === 65){ //LEFT
-        moves.left = move;
-    }else if(keyCode === 83){ //DOWN
-        moves.down = move;
-    }else if(keyCode === 68){ //RIGHT
-        moves.right = move;
-    }
+function setMove(keyCode, move) {
+  if (keyCode === 87) {
+    //UP
+    moves.up = move;
+  } else if (keyCode === 65) {
+    //LEFT
+    moves.left = move;
+  } else if (keyCode === 83) {
+    //DOWN
+    moves.down = move;
+  } else if (keyCode === 68) {
+    //RIGHT
+    moves.right = move;
+  }
 }
 
-function checkKeys(){
-    if(moves.up && moves.right) processKeyPress({x: 1, y: -1});
-    else if(moves.up && moves.left) processKeyPress({x: -1, y:-1});
-    else if(moves.down && moves.right) processKeyPress({x: 1, y:1});
-    else if(moves.down && moves.left) processKeyPress({x: -1, y: 1});
-    else if(moves.up) processKeyPress({x: 0, y: -1});
-    else if(moves.left) processKeyPress({x: -1, y: 0});
-    else if(moves.down) processKeyPress({x: 0, y: 1});
-    else if(moves.right) processKeyPress({x: 1, y: 0});
+function checkKeys() {
+  if (moves.up && moves.right) processKeyPress({ x: 1, y: -1 });
+  else if (moves.up && moves.left) processKeyPress({ x: -1, y: -1 });
+  else if (moves.down && moves.right) processKeyPress({ x: 1, y: 1 });
+  else if (moves.down && moves.left) processKeyPress({ x: -1, y: 1 });
+  else if (moves.up) processKeyPress({ x: 0, y: -1 });
+  else if (moves.left) processKeyPress({ x: -1, y: 0 });
+  else if (moves.down) processKeyPress({ x: 0, y: 1 });
+  else if (moves.right) processKeyPress({ x: 1, y: 0 });
 }
 
-function drawEdges(){
-    //horizontal
-    for(let i = 0; i < dimension; i += diff){
-        fill(255);
-
-        //top
-        drawBlock(i + diff/2, diff/2, diff);
-
-        //down
-        drawBlock(i + diff/2, dimension - diff/2, diff);
-    }
-
-    //veritcal
-    for(let i = 0; i < dimension; i += diff){
-        fill(255);
-
-        //left
-        drawBlock(diff/2, i + diff/2, diff);
-
-        //right
-        drawBlock(dimension - diff/2, i + diff/2, diff);
-    }
+function mouseClicked() {
+  triggerShot(mouseX, mouseY);
 }
 
-function drawBlock(x, y, width){
-    const half = width / 2;
-
+function drawEdges() {
+  //horizontal
+  for (let i = 0; i < dimension; i += diff) {
     fill(255);
-    quad(x - half, y - half, x + half, y - half, x + half, y + half, x - half, y + half);
-}
 
-function drawTitle(){
-    const field = "Team Battle";
+    //top
+    drawBlock(i + diff / 2, diff / 2, diff);
 
+    //down
+    drawBlock(i + diff / 2, dimension - diff / 2, diff);
+  }
+
+  //veritcal
+  for (let i = 0; i < dimension; i += diff) {
     fill(255);
-    textSize(32);
-    text(field, dimension+15, 32);
+
+    //left
+    drawBlock(diff / 2, i + diff / 2, diff);
+
+    //right
+    drawBlock(dimension - diff / 2, i + diff / 2, diff);
+  }
 }
 
-function drawBattleField(){
-    for(let i = 0; i < battleField.length; i++){
-        drawBlock(battleField[i].x, battleField[i].y, diff);
-    }
+function drawBlock(x, y, width) {
+  const half = width / 2;
+
+  fill(255);
+  quad(
+    x - half,
+    y - half,
+    x + half,
+    y - half,
+    x + half,
+    y + half,
+    x - half,
+    y + half
+  );
 }
 
-function drawPlayer(player){
-    if(player.team === 1) fill(color(255,0,0));
-    else fill(color(0,255,255));
+function drawTitle() {
+  const field = "Team Battle";
 
-    ellipse(player.x, player.y, 20);
-    textSize(14);
-    text(player.name, player.x+5, player.y-12);
+  fill(255);
+  textSize(32);
+  text(field, dimension + 15, 32);
+}
+
+function drawBattleField() {
+  for (let i = 0; i < battleField.length; i++) {
+    drawBlock(battleField[i].x, battleField[i].y, diff);
+  }
+}
+
+function drawPlayer(player) {
+  if (player.team === 1) fill(color(255, 0, 0));
+  else fill(color(0, 255, 255));
+
+  ellipse(player.x, player.y, 20);
+  textSize(14);
+  text(player.name, player.x + 5, player.y - 12);
+}
+
+function drawBullets() {
+  for (let i = 0; i < bullets.length; i++) {
+    if (bullets[i].team === 1) fill(color(255, 0, 0));
+    else fill(color(0, 255, 255));
+
+    ellipse(bullets[i].x, bullets[i].y, 5);
+  }
 }
 
 //SOCKET STUFF
-function processKeyPress(dirs){
-    socket.emit('mouseProcess', dirs);
+function processKeyPress(dirs) {
+  socket.emit("mouseProcess", dirs);
 }
 
-function processNameAndTeam(name, team){
-    socket.emit('processNameAndTeam', {name, team});
+function processNameAndTeam(name, team) {
+  socket.emit("processNameAndTeam", { name, team });
 }
 
-socket.on('battleField', (data) => {
-    battleField = data;
+function triggerShot(x, y) {
+  socket.emit("triggerShot", { x, y });
+}
+
+socket.on("battleField", data => {
+  battleField = data;
 });
 
-socket.on('me', (data) => {
-   me = data;
+socket.on("me", data => {
+  me = data;
 });
 
-socket.on('otherPlayers', (data) => {
-    otherPlayers = data;
+socket.on("otherPlayers", data => {
+  otherPlayers = data;
+});
+
+socket.on("bullets", data => {
+  bullets = data;
 });
--- server/Game.js
+++ server/Game.js
@@ -1,105 +1,182 @@
-const getDistance = require('./commonOperations').getDistance;
+const getDistance = require("./commonOperations").getDistance;
 
-class Game{
-    constructor(app){
-        this.server = require('http').createServer(app);
-        this.io = require('socket.io')(this.server);
-        this.PORT = process.env.PORT || 3000;
+class Game {
+  constructor(app) {
+    this.server = require("http").createServer(app);
+    this.io = require("socket.io")(this.server);
+    this.PORT = process.env.PORT || 3000;
 
-        this.battleField = require('./data/battlefield');
+    this.battleField = require("./data/battlefield");
+    this.bullets = [];
 
-        this.SOCKET_LIST = {};
-        this.PLAYER_LIST = {};
-        this.id = 1;
-    }
+    this.SOCKET_LIST = {};
+    this.PLAYER_LIST = {};
+    this.id = 1;
+  }
 
-    runServer(){
-        this.server.listen(this.PORT, () => {
-            console.log('Server running on port: ' + this.PORT);
-        })
-    }
+  runServer() {
+    this.server.listen(this.PORT, () => {
+      console.log("Server running on port: " + this.PORT);
+    });
+  }
 
-    ioON(){
-        this.io.sockets.on('connection', (socket) => {
-                //handle connection
-            socket.id = this.id;
-            this.id++;
-            this.SOCKET_LIST[socket.id] = socket;
-            console.log('Socket connection with id: ' + socket.id);
+  ioON() {
+    this.io.sockets.on("connection", socket => {
+      //handle connection
+      socket.id = this.id;
+      this.id++;
+      this.SOCKET_LIST[socket.id] = socket;
+      console.log("Socket connection with id: " + socket.id);
 
-            socket.emit('battleField', this.battleField);
+      socket.emit("battleField", this.battleField);
 
-            socket.on('mouseProcess', (data) => {
-                this.movePlayer(socket.id, data);
-            });
+      socket.on("mouseProcess", data => {
+        this.movePlayer(socket.id, data);
+      });
 
-            socket.on('disconnect', () => {
-                delete this.PLAYER_LIST[socket.id];
-                delete this.SOCKET_LIST[socket.id];
-                console.log('Socket dissconnected with id: ' + socket.id);
-            });
+      socket.on("triggerShot", data => {
+        this.setShot(data.x, data.y, this.PLAYER_LIST[socket.id]);
+      });
 
-            socket.on('processNameAndTeam', (data) => {
-                this.setNewPlayer(data.team, socket.id, data.name);
-            });
-        });
+      socket.on("disconnect", () => {
+        delete this.PLAYER_LIST[socket.id];
+        delete this.SOCKET_LIST[socket.id];
+        console.log("Socket dissconnected with id: " + socket.id);
+      });
 
-            //60FPS
-        setInterval(() => {
-            for(let pi in this.PLAYER_LIST){
-                const player = this.PLAYER_LIST[pi];
-                const socket = this.SOCKET_LIST[player.id];
+      socket.on("processNameAndTeam", data => {
+        this.setNewPlayer(data.team, socket.id, data.name);
+      });
+    });
 
-                //my info
-                socket.emit('me', player);
+    //60FPS
+    setInterval(() => {
+      this.processBullets();
+      for (let pi in this.PLAYER_LIST) {
+        const player = this.PLAYER_LIST[pi];
+        const socket = this.SOCKET_LIST[player.id];
 
-                //other players
-                let otherPlayers = [];
-                for(let pi2 in this.PLAYER_LIST){
-                    if(pi === pi2) continue;
-                    const player2 = this.PLAYER_LIST[pi2];
+        //my info
+        socket.emit("me", player);
 
-                    otherPlayers.push(player2);
-                }
-                socket.emit('otherPlayers', otherPlayers);
-            }
-        }, 1000/60);
-    }
+        //other players
+        let otherPlayers = [];
+        for (let pi2 in this.PLAYER_LIST) {
+          if (pi === pi2) continue;
+          const player2 = this.PLAYER_LIST[pi2];
 
-    setNewPlayer(team, id, name){
-        const player = {
-            team,
-            id,
-            hp: 100,
-            ammo: 10,
-            x: 400,
-            y: team === 1 ? 100 : 700,
-            name
-        };
-
-        this.PLAYER_LIST[id] = player;
-    }
-
-    movePlayer(id, dirs){
-        const newX = this.PLAYER_LIST[id].x + dirs.x;
-        const newY = this.PLAYER_LIST[id].y + dirs.y;
-
-        let possible = true;
-
-        //check edges
-        if(newX <= 19 || newX >= 781 || newY <= 19 || newY >= 781) possible = false;
-
-        //check battleField
-        for(let i = 0; i < this.battleField.length; i++){
-            const dist = getDistance(newX, newY, this.battleField[i].x, this.battleField[i].y);
-            if(dist <= 16) possible = false;
+          otherPlayers.push(player2);
         }
+        socket.emit("otherPlayers", otherPlayers);
+        socket.emit("bullets", this.bullets);
+      }
+    }, 1000 / 60);
+  }
 
-        if(possible){
-            this.PLAYER_LIST[id].x = newX;
-            this.PLAYER_LIST[id].y = newY;
-        }
+  setNewPlayer(team, id, name) {
+    const player = {
+      team,
+      id,
+      hp: 100,
+      ammo: 10,
+      x: 400,
+      y: team === 1 ? 100 : 700,
+      name
+    };
+
+    this.PLAYER_LIST[id] = player;
+  }
+
+  movePlayer(id, dirs) {
+    const newX = this.PLAYER_LIST[id].x + dirs.x;
+    const newY = this.PLAYER_LIST[id].y + dirs.y;
+
+    let possible = true;
+
+    //check edges
+    if (newX <= 19 || newX >= 781 || newY <= 19 || newY >= 781)
+      possible = false;
+
+    //check battleField
+    for (let i = 0; i < this.battleField.length; i++) {
+      const dist = getDistance(
+        newX,
+        newY,
+        this.battleField[i].x,
+        this.battleField[i].y
+      );
+      if (dist <= 16) possible = false;
     }
+
+    if (possible) {
+      this.PLAYER_LIST[id].x = newX;
+      this.PLAYER_LIST[id].y = newY;
+    }
+  }
+
+  setShot(x, y, player) {
+    const angle = Math.atan2(x - player.x, y - player.y);
+    const xSpeed = Math.sin(angle);
+    const ySpeed = Math.cos(angle);
+
+    this.bullets.push({
+      xSpeed,
+      ySpeed,
+      x: player.x,
+      y: player.y,
+      team: player.team
+    });
+  }
+
+  processBullets() {
+    let newBullets = [];
+    for (let i = 0; i < this.bullets.length; i++) {
+      let bullet = this.bullets[i];
+      bullet.x += bullet.xSpeed * 5;
+      bullet.y += bullet.ySpeed * 5;
+
+      let clear = true;
+
+      if (
+        bullet.x >= 790 ||
+        bullet.x <= 10 ||
+        bullet.y >= 790 ||
+        bullet.y <= 10
+      ) {
+        clear = false;
+      }
+
+      for (let j = 0; j < this.battleField.length; j++) {
+        const dist = getDistance(
+          bullet.x,
+          bullet.y,
+          this.battleField[j].x,
+          this.battleField[j].y
+        );
+        if (dist <= 7) clear = false;
+      }
+
+      for (var pi in this.PLAYER_LIST) {
+        const player = this.PLAYER_LIST[pi];
+        if (player.team === bullet.team) continue;
+
+        const dist = getDistance(bullet.x, bullet.y, player.x, player.y);
+
+        if (dist <= 12) {
+          clear = false;
+          this.processHit(pi);
+          break;
+        }
+      }
+
+      if (clear) newBullets.push(bullet);
+    }
+    this.bullets = newBullets;
+  }
+
+  processHit(pi) {
+    console.log(this.PLAYER_LIST[pi]);
+  }
 }
 
 module.exports = Game;
